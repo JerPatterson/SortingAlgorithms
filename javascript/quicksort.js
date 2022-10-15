9a9e0e40635f9fe1d7dd1ff046f08060d1176678
JerPatterson <jeremie.patterson@polymtl.ca>
2022-10-14 20:17:47 -0400

    implements quicksort in a better way in javascript

--- javascript/quicksort.js
+++ javascript/quicksort.js
@@ -1,13 +1,13 @@
 import basicsort from "./basicsort.js";
 import { generateRandomArray } from "./test.js"
-import { switchValues, getPivotIndex } from "./utils.js"
+import { getPivotIndex } from "./utils.js";
 
 
 function main() {
     let t1, t2;
 
-    let array = generateRandomArray(30);
-    let copyArray = array.filter(_ => { return true; })
+    const array = generateRandomArray(1000);
+    const copyArray = array.filter(_ => { return true; })
 
     t1 = performance.now();
     basicsort(copyArray);
@@ -21,36 +21,36 @@ function main() {
 }
 
 
-function quicksort(table, start, end) {
-    const length = end - start + 1;
-
-    if (length > 2) {
-        switchValues(table, start - 1 + getPivotIndex(length), end);
-        const pivot = table[end];
-
-        let left = start;
-        let right = end - 1;
-        while (left < right) {
-            while (table[left] < pivot && left < right) { ++left; }
-            while (table[right] > pivot && left < right) { --right; }
-
-            if (left < right)
-                switchValues(table, left, right);
-        }
-
-        if (table[left] < pivot)
-            ++left;
-        
-        switchValues(table, end, left);
-        quicksort(table, start, (left - 1 > 0) ? left - 1 : 0);
-        quicksort(table, (left + 1 < end) ? left + 1 : end, end);
+function quicksort(table) {
+    if (table.length < 2) {
         return table;
     }
 
-    else if (length == 2) {
-        if (table[start] > table[end])
-            switchValues(table, start, end);
+    const pivot_index = getPivotIndex(table.length);
+    const pivot = table[pivot_index];
+
+    const left = Array(0);
+    const right = Array(0);
+
+    for (let i = 0; i < table.length; ++i) {
+        if (i != pivot_index) {
+            if (table[i] <= pivot) {
+                left.push(table[i]);
+            }
+            else {
+                right.push(table[i]);
+            }
+        }
     }
+
+    quicksort(left);
+    quicksort(right);
+    
+    table = left;
+    table.push(pivot);
+    table.concat(right);
+
+    return table;
 }
 
 
---
+++ javascript/single_array_quicksort.js
@@ -0,0 +1,57 @@
+import basicsort from "./basicsort.js";
+import { generateRandomArray } from "./test.js"
+import { switchValues, getPivotIndex } from "./utils.js"
+
+
+function main() {
+    let t1, t2;
+
+    let array = generateRandomArray(30);
+    let copyArray = array.filter(_ => { return true; })
+
+    t1 = performance.now();
+    basicsort(copyArray);
+    t2 = performance.now();
+    console.log(copyArray, t2 - t1);
+
+    t1 = performance.now();
+    quicksort(array, 0, array.length - 1);
+    t2 = performance.now();
+    console.log(array, t2 - t1);
+}
+
+
+function quicksort(table, start, end) {
+    const length = end - start + 1;
+
+    if (length > 2) {
+        switchValues(table, start - 1 + getPivotIndex(length), end);
+        const pivot = table[end];
+
+        let left = start;
+        let right = end - 1;
+        while (left < right) {
+            while (table[left] < pivot && left < right) { ++left; }
+            while (table[right] > pivot && left < right) { --right; }
+
+            if (left < right)
+                switchValues(table, left, right);
+        }
+
+        if (table[left] < pivot)
+            ++left;
+        
+        switchValues(table, end, left);
+        quicksort(table, start, (left - 1 > 0) ? left - 1 : 0);
+        quicksort(table, (left + 1 < end) ? left + 1 : end, end);
+        return table;
+    }
+
+    else if (length == 2) {
+        if (table[start] > table[end])
+            switchValues(table, start, end);
+    }
+}
+
+
+main()
