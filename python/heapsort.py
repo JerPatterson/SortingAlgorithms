73bb3b5271419b689250b82f30f1e498d00c9430
JerPatterson <jeremie.patterson@polymtl.ca>
2022-11-14 21:28:22 -0500

    implements heapsort

--- python/heapsort.py
+++ python/heapsort.py
@@ -5,7 +5,7 @@ def main():
     for _ in range(1):
         test_table = generateRandomTable(20)
         print(test_table)
-        test_table = Heap.sort(test_table)
+        test_table = Heap.sort(Heap(), test_table)
         print(test_table)
 
     
@@ -15,19 +15,47 @@ class Heap:
 
 
     def heapify(self, table) -> None:
-        pass
+        self.array = [0] + table
+        for i in range(len(table) // 2, 0, -1):
+            self.percolateDown(i)
+
+        print(self.array)
 
 
-    def percolateDown(self, hole: int):
-        pass
+    def percolateDown(self, hole: int) -> None:
+        temp = self.array[hole]
+
+        while hole * 2 < len(self.array) - 1:
+            child = hole * 2
+            if child < len(self.array) - 1 and self.array[child] > self.array[child + 1]:
+                child += 1
+            if temp > self.array[child]:
+                self.array[hole] = self.array[child]
+            else:
+                break
+            hole = child
+
+        self.array[hole] = temp
 
 
-    def sort(self) -> List[int]:
-        pass
+    def sort(self, table) -> List[int]:
+        self.heapify(table)
+        for i in range(len(table)):
+            table[i] = self.delete()
+
+        return table
 
 
     def delete(self) -> int:
-        pass
+        root = self.array[1]
+        last_element = self.array[len(self.array) - 1]
+        self.array = self.array[:-1]
+
+        if len(self.array) > 2:
+            self.array[1] = last_element
+            self.percolateDown(1)
+
+        return root
 
 
 if __name__ == "__main__":
